587d4513068d69e36f00dff06768b3a0a4066aa6
vladshiligin <vlad.shiligin@ons.gov.uk>
2018-12-20 16:54:14 +0000

    report done for reporting units, local units, legal units

--- src/main/scala/action/InputAnalyser.scala
+++ src/main/scala/action/InputAnalyser.scala
@@ -58,7 +58,6 @@ object InputAnalyser extends RddLogging{
     spark.createDataFrame(tuRows, louRowSchema)
   }
 
-
   def getLegalUnitDF(appconf:AppParams)(implicit spark: SparkSession): DataFrame =  {
     val leuRows:RDD[Row] = HBaseDao.readTable(appconf,Configs.conf, HBaseDao.leusTableName(appconf)).map(_.toLeuRow)
     spark.createDataFrame(leuRows, linksLeuRowSchema)
@@ -121,18 +120,18 @@ object InputAnalyser extends RddLogging{
     val ru_less_lous_report = toReportEntry(ru_less_lous,"Local Unit", "LOU","lurn","rurn","Reporting Unit","REU",appconf.PERIOD,"CHILDLESS", "Local Unit: No REU children found in REU table")
     //lous without ru on links level
     val ru_less_lous_links = louLinks.join(ruLinks,Seq("rurn"), "left_anti")
-    val ru_less_lous_links_report = toReportEntry(ru_less_lous,"Local Unit", "LINKS","lurn","rurn","Reporting Unit","LINKS",appconf.PERIOD,"CHILDLESS", "Local Unit: No REU children found in LINKS table")
+    val ru_less_lous_links_report = toReportEntry(ru_less_lous_links,"Local Unit", "LINKS","lurn","rurn","Reporting Unit","LINKS",appconf.PERIOD,"CHILDLESS", "Local Unit in LINKS: No REU children found in LINKS table")
+
     //lous without parent Enterprise on table level
     val ent_less_lous = lous.join(ents,Seq("ern"), "left_anti")
-
+    val ent_less_lous_report = toReportEntry(ent_less_lous,"Local Unit", "LOU","lurn","ern","Enterprise Unit","ENT",appconf.PERIOD,"ORPHAN", "Local Unit: No parent ENTERPRISE found in ENTERPRISE table")
     //lous without parent Enterprise on links level
     val ent_less_lous_links = louLinks.join(entLinks.drop("leus").drop("rus").withColumn("lurn", explode_outer(col("lous"))),Seq("ern"), "left_anti")
-
+    val ent_less_lous_links_report = toReportEntry(ent_less_lous_links,"Local Unit", "LINKS","lurn","ern","ENTERPRISE","LINKS",appconf.PERIOD,"ORPHAN", "Local Unit in LINKS: No parent ENTERPRISE found in LINKS table")
     //lous links refer to non-existing rous (table) entities
-    val lous_links_with_fantom_rous_parents = louLinks.join(ents, Seq("ern"), "left_anti")
+    val ent_link_less_lous_links = louLinks.join(ents, Seq("ern"), "left_anti")
+    val ent_link_less_lous_links_report = toReportEntry(ent_link_less_lous_links,"Local Unit", "LINKS","lurn","ern","ENTERPRISE","LINKS",appconf.PERIOD,"ORPHAN", "Local Unit in LINKS: No parent ENTERPRISE found in ENTERPRISE table")
 
-    //lous links refer to non-existing rous (table) entities
-    val lous_links_with_fantom_ents_parents = louLinks.join(rus, Seq("rurn"), "left_anti")
 
 
 
@@ -141,31 +140,36 @@ object InputAnalyser extends RddLogging{
       * */
     //rus without parent Enterprise on table level
     val ent_less_rus = rus.join(ents, Seq("ern"),"left_anti")
+    val ent_less_rus_report = toReportEntry(ent_less_rus,"Reporting Unit", "REU","rurn","ern","Enterprise Unit","ENTERPRISE",appconf.PERIOD,"ORPHAN", "Reporting Unit: No parent ENTERPRISE found in ENTERPRISE table")
     //rus without parent Enterprise on links level
     val ent_less_rus_links = ruLinks.join(entLinks.drop("leus").drop("lous").withColumn("rurn", explode_outer(col("rus"))),Seq("ern"), "left_anti")
-
-    //rus linked to non-existing LOUs children
-    val rus_fantom_lous_children_links = ruLinks.withColumn("lurn", explode_outer(col("lous"))).join(louLinks, Seq("ern"),"left_anti")
-
+    val ent_less_rus_links_report = toReportEntry(ent_less_rus_links,"Reporting Unit", "LINKS","rurn","ern","Enterprise Unit","LINKS",appconf.PERIOD,"ORPHAN", "Local Unit in LINKS: No parent ENTERPRISE found in LINKS table")
+    //rus linked to non-existing LOUs children on LINKS table level
+    val rus_links_with_fantom_lous_children_links = ruLinks.withColumn("lurn", explode_outer(col("lous"))).join(louLinks, Seq("rurn"),"left_anti")
+    val rus_links_with_fantom_lous_children_links_report = toReportEntry(rus_links_with_fantom_lous_children_links,"Reporting Unit", "LINKS","rurn","lurn","Local Unit","LINKS",appconf.PERIOD,"CHILDLESS", "Reporting Unit in LINKS: No Local Unit children found in LINKS table")
     //REUs records in Links referring to non-existing enterprise units in ENT table
     val rus_links_with_fantom_ent_parents = ruLinks.drop("lous").join(ents,Seq("ern"), "left_anti")
-
+    val rus_links_with_fantom_ent_parents_report = toReportEntry(rus_links_with_fantom_ent_parents,"Reporting Unit", "LINKS","rurn","ern","ENTERPRISE","ENT",appconf.PERIOD,"ORPHAN", "Reporting Unit in LINKS table: No parent ENTERPRISE found in ENTERPRISE table")
 
     //REUs records in Links referring to non-existing enterprise units in ENT table
     val rus_links_with_fantom_lu_children = ruLinks.withColumn("lurn", explode_outer(col("lous"))).join(lous,Seq("lurn"),"left_anti")
+    val rus_links_with_fantom_lu_children_report = toReportEntry(rus_links_with_fantom_lu_children,"Reporting Unit", "LINKS","rurn","lurn","Local Unit","LOU",appconf.PERIOD,"CHILDLESS", "Reporting Unit in LINKS: No parent Reporting Unit found in REU table")
 
 
 
 
-  /**
+    /**
     * Legal Units
     * */
     val not_linked_leus = leus.join(entLinks, Seq("ern"),"left_anti")
-    val no_table_ref_leus_links = entLinks.join(leus, Seq("ern"),"left_anti")
+    val not_linked_leus_report = toReportEntry(not_linked_leus,"Legal Unit", "LEU","ubrn","ern","Enterprise Unit","LINKS",appconf.PERIOD,"ORPHAN", "Legal Unit in LEU table: No parent ENTERPRISE found in LINKS table")
+
     val ent_less_leus_links = leuLinks.join(entLinks.drop("rus").drop("lous").withColumn("ubrn", explode_outer(col("leus"))),Seq("ern"), "left_anti")
+    val ent_less_leus_links_report = toReportEntry(ent_less_leus_links,"Legal Unit", "LINKS","ubrn","ern","Enterprise Unit","LINKS",appconf.PERIOD,"ORPHAN", "Legal Unit in LINKS: No parent ENTERPRISE found in LINKS table")
+
 
     val ent_less_leus = leus.join(ents, Seq("ern"),"left_anti")
-
+    val ent_less_leus_report = toReportEntry(ent_less_leus,"Legal Unit", "LEU","ubrn","ern","Enterprise Unit","ENTERPRISE",appconf.PERIOD,"ORPHAN", "Legal Unit in LEU table: No parent ENTERPRISE found in ENTERPRISE table")
 
 
 
@@ -176,6 +180,7 @@ object InputAnalyser extends RddLogging{
     //ents not registered in links
    val not_linked_ents = ents.join(entLinks,Seq("ern"),"left_anti")
    val no_table_ref_ent_links = entLinks.join(ents,Seq("ern"),"left_anti")
+   val no_table_ref_leus_links = entLinks.join(leus, Seq("ern"),"left_anti")
     //on ENT table level
    val ents_with_fantom_leu_children = ents.join(leus, Seq("ern"),"left_anti")
    val ents_with_fantom_ru_children = ents.join(rus, Seq("ern"),"left_anti")
